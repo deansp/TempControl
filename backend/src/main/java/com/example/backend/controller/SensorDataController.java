ffce04e7ecba9aed587a603230bc60ec67633740
deanspecht <deansp@web.de>
2024-05-07 11:51:10 +0200

    get mapping to mongoDB

--- backend/src/main/java/com/example/backend/controller/SensorDataController.java
+++ backend/src/main/java/com/example/backend/controller/SensorDataController.java
@@ -1,11 +1,25 @@
 package com.example.backend.controller;
 
+import com.example.backend.model.SensorData;
+import com.example.backend.service.SensorDataService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/sensordata")
 @RequiredArgsConstructor
 public class SensorDataController {
+
+    private final SensorDataService service;
+
+    @GetMapping
+    public List<SensorData> getSensorData() {
+        return service.getAllSensorData();
+    }
+
+
 }
--- backend/src/main/java/com/example/backend/service/SensorDataService.java
+++ backend/src/main/java/com/example/backend/service/SensorDataService.java
@@ -1,9 +1,17 @@
 package com.example.backend.service;
 
+import com.example.backend.model.SensorData;
+import com.example.backend.repository.SensorDataRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class SensorDataService {
+
+    private final SensorDataRepository repo;
+    public List<SensorData> getAllSensorData() {return repo.findAll();}
+
 }
