0c26b3c4745910081c717ef0dc7b14e176839b2d
deanspecht <deansp@web.de>
2024-05-07 11:30:04 +0200

    init mongoDB

--- backend/src/main/java/com/example/backend/repository/SensorDataRepository.java
+++ backend/src/main/java/com/example/backend/repository/SensorDataRepository.java
@@ -1,7 +1,9 @@
 package com.example.backend.repository;
 
+import com.example.backend.model.SensorData;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class SensorDataRepository {
+public interface SensorDataRepository extends MongoRepository<SensorData, String> {
 }
